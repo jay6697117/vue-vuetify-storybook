cab90c2af99763cd0f726e852deb90d8ac084f19
Max Hodges <Max@whiterabbitjapan.com>
2019-02-11 21:40:56 +0900

    update config

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -6,9 +6,6 @@ import Vuetify, { VApp } from 'vuetify/lib';
 import 'vuetify/src/stylus/app.styl';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
-// index.js or main.js
-import 'vuetify/dist/vuetify.min.css'; // Ensure you are using css-loader
-
 Vue.use(Vuetify, {
   iconfont: 'md'
 })
